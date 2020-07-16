4fd5e6355dd30b3b3e13f2b834a9145025f5e0a1
JulinaM <julinamaharjan@gmail.com>
2020-07-16 18:03:59 -0400

    dtype added before sql insert

--- dbModels/MySQLModel.py
+++ dbModels/MySQLModel.py
@@ -1,10 +1,14 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+
+
 def store_df_sql(df, tableName):
-    from sqlalchemy import create_engine
     connectionString = "mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root", pw="password", db="census2020")
     engine = create_engine(connectionString)
     con = engine.connect()
     try:
-        df.to_sql(tableName, con, if_exists='append', chunksize=1000)
+        dtype_dict = get_sql_col(df)
+        df.to_sql(tableName, con, if_exists='append', index=False, dtype=dtype_dict, chunksize=1000)
     except ValueError as vx:
         print(vx)
     except Exception as ex:
@@ -13,6 +17,23 @@ def store_df_sql(df, tableName):
         print("Table %s created successfully." % tableName);
     finally:
         con.close()
+
+
+def get_sql_col(df):
+    dtypedict = {}
+    for i, j in zip(df.columns, df.dtypes):
+        if "object" in str(j):
+            dtypedict.update({i: sqlalchemy.types.NVARCHAR(length=255)})
+
+        if "datetime" in str(j):
+            dtypedict.update({i: sqlalchemy.types.DateTime()})
+
+        if "float" in str(j):
+            dtypedict.update({i: sqlalchemy.types.Float(precision=3, asdecimal=True)})
+
+        if "int" in str(j):
+            dtypedict.update({i: sqlalchemy.types.INT()})
+    return dtypedict
 
 
 class MySQLModel:
